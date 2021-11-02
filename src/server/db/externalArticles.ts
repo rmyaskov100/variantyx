dcbfffff48ae7551d1dbd79adf709e9cfdc9cca6
Ruslan Myaskovskiy <russ.myaskovskiy@gmail.com>
2021-11-02 00:43:46 -0700

    initial project

--- src/server/db/externalArticles.ts
+++ src/server/db/externalArticles.ts
@@ -3,7 +3,7 @@ import { Connection } from './index';
 export const all = async () => {
     return new Promise((resolve, reject) => {
 
-        Connection.query('SELECT * FROM variantyx_project', (err, results) => {
+        Connection.query('SELECT * FROM externalArticles', (err, results) => {
             if(err) {
                 return reject(err);
             }
